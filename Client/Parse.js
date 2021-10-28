88189dde5984beface5ef6ed9daf3882cd29ced2
Thistleman <codes5096@gmail.com>
2021-10-27 20:10:00 -0400

    Updated Parse, added config example to be completed, fixed ESLint rule

--- Client/Parse.js
+++ Client/Parse.js
@@ -1,1 +1,29 @@
-//Connect to server
+import $ from 'jquery';
+
+import tokens from '../config';
+
+const Parse = {
+
+  getStuff: {
+    sUrl: `https://app-hrsei-api.herokuapp.com/api/fec2/${tokens.campus}/`,
+
+    getBlogs: (callback) => {
+      $.ajax({
+        url: Parse.sUrl,
+        method: 'GET',
+        dataType: 'json',
+        beforeSend(xhr) {
+          /* Authorization header */
+          xhr.setRequestHeader('Authorization', `${tokens.gitToken}`);
+        },
+        success: (data) => {
+          console.log('success!', data);
+          // callback(data);
+        },
+        error: (error) => {
+          console.log('you done goofed!', error);
+        },
+      });
+    },
+  },
+};
---
+++ config.example.js
@@ -0,0 +1,8 @@
+// rename to congig.js and uncomment the export, fill in with your github token
+
+const tokens = {
+  campus: 'hr-sfo',
+  gitToken: 'fill_me_in',
+};
+
+// export default tokens;
