d4c4759d9924e3e3a36ac17d91c1d81744e4ec33
Thistleman <codes5096@gmail.com>
2021-10-29 14:11:41 -0400

    Fixed Parse call to server, refactored App to use hooks

--- Client/SRC/App.jsx
+++ Client/SRC/App.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import Parse from '../Parse';
+import React, { useState, useEffect } from 'react';
+import Parse from '../Parse.js';
 
-import RatingsNReviews from './Components/RatingsNReviews/RatingsNReviews.jsx'
+import RatingsNReviews from './Components/RatingsNReviews/RatingsNReviews.jsx';
 import Overview from './Components/DOverview/Overview.jsx';
-import RIAC from './Components/DKProdComp/RIAC.jsx'
-import QAMain from './Components/YQandA/QAMain.jsx'
+import RIAC from './Components/DKProdComp/RIAC.jsx';
+import QAMain from './Components/YQandA/QAMain.jsx';
 
-const App = (props) => (
-  <div>
-    <div>Hello World</div>
-    <Overview />
-    <RatingsNReviews />
-    <RIAC/>
-    <QAMain />
-  </div>
-);
+function App(props) {
+  // example of using a hook for state
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // testing parse
+    // console.log(Parse);
+    Parse.getStuff();
+  });
+
+  return (
+    <div>
+      <div>Hello World</div>
+      <Overview />
+      <RatingsNReviews />
+      <RIAC />
+      <QAMain />
+    </div>
+  );
+}
 
 export default App;
