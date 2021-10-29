7c600e0a2f85d33e330edab854f27edffda93a32
Norman Fung <itsnormsyo@gmail.com>
2021-10-29 12:55:15 -0700

    syntax error fix running npm start can render page on localhost/3000

--- Client/SRC/Components/RatingsNReviews/RatingsNReviews.jsx
+++ Client/SRC/Components/RatingsNReviews/RatingsNReviews.jsx
@@ -4,8 +4,14 @@ import React, { useEffect, useState } from 'react';
 const RatingsNReviews = () => {
 
   return (
-
+<>
     <h1> Hello From Ratings And Reviews </h1>
+
+    <div className="RatingsNReviews">
+
+    </div>
+
+    </>
   )
 }
 
