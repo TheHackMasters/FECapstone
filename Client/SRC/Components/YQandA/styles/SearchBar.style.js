ddc35d9f7c6e0a89d5d236fdbe622e0703902c06
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2021-10-29 16:23:54 -0500

    CSS updated for SearchBar

--- Client/SRC/Components/YQandA/styles/SearchBar.style.js
+++ Client/SRC/Components/YQandA/styles/SearchBar.style.js
@@ -4,14 +4,14 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   font-size: 1em;
-  height: 30px;
+  height: 20px;
   width: 700px;
   border: none
 `
 const Form = styled.form`
   border: 1px solid black;
-  width: 750px;
-  height: 40px
+  width: 730px;
+  height: 30px
 `
 
 export {Input, Form};
