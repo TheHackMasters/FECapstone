c593771d98a04927e29e4a33e45379d798749b9c
Norman Fung <itsnormsyo@gmail.com>
2021-10-30 09:55:07 -0700

    hardcoded outline for components

---
+++ Client/SRC/Components/RatingsNReviews/Ratings.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+
+const Ratings = () => {
+
+
+
+  return (
+    <>
+      <h1>Hello from ratings</h1>
+
+      <div>
+
+
+        <div className="ratings">
+          <div>RATINGS </div>
+          <div>3.5 *****</div>
+          <div>100% of reviewers recommend this product</div>
+          <div>5 stars ##########</div>
+          <div>4 stars #####</div>
+          <div>3 stars ########</div>
+          <div>2 stars ##</div>
+          <div>1 stars ###</div>
+          <div> Size (too small, too large)</div>
+          <div> Comfort (poor, perfect)</div>
+        </div>
+
+      </div>
+
+    </>
+  )
+}
+
+export default Ratings;
