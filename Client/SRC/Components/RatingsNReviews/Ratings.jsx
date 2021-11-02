2e0bd749980241580406b24ee191f48efd54c1b3
Norman Fung <itsnormsyo@gmail.com>
2021-11-02 15:41:15 -0700

    rendering api data using get requests in R n R main

--- Client/SRC/Components/RatingsNReviews/Ratings.jsx
+++ Client/SRC/Components/RatingsNReviews/Ratings.jsx
@@ -1,33 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const Ratings = () => (
-  <>
-    <RatingsDiv>
+const Ratings = (props) => {
+  // const { data: { ratings, characteristics, recommended } } = meta;
+  // console.log(props);
+  // console.log(ratings);
 
-      <h1>Hello from ratings</h1>
+  // const [ratings, setRatings] = useState();
 
-      <div>
+  let recValue = 0;
 
-        <div className="ratings">
-          <div>RATINGS </div>
-          <div>3.5 *****</div>
-          <div>100% of reviewers recommend this product</div>
-          <div>5 stars ##########</div>
-          <div>4 stars #####</div>
-          <div>3 stars ########</div>
-          <div>2 stars ##</div>
-          <div>1 stars ###</div>
-          <div> Size (too small, too large)</div>
-          <div> Comfort (poor, perfect)</div>
+  if (props.meta !== undefined) {
+    const { meta } = props;
+    // console.log(meta);
+    const rc = meta.data.recommended;
+    const recommended = Math.round((Number(rc.true) / (Number(rc.false) + Number(rc.true))) * 100);
+    recValue = recommended;
+    // console.log(recValue);
+  }
+
+
+
+  return (
+
+    <>
+      <RatingsDiv>
+
+        <div>
+
+          <div className="ratings">
+            <div>Ratings & Reviews </div>
+            <div>3.5 *****</div>
+            <div>
+              {' '}
+              {recValue}
+              % of reviewers recommend this product
+            </div>
+            <div>5 stars ##########</div>
+            <div>4 stars #####</div>
+            <div>3 stars ########</div>
+            <div>2 stars ##</div>
+            <div>1 stars ###</div>
+            <div> Size (too small, too large)</div>
+            <div> Comfort (poor, perfect)</div>
+          </div>
+
         </div>
 
-      </div>
+      </RatingsDiv>
 
-    </RatingsDiv>
-
-  </>
-);
+    </>
+  );
+};
 
 export default Ratings;
 
