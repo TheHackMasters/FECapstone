1a9f5effc70f4a209e826e45b9be4d7eac085999
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2021-11-08 13:21:19 -0600

    QAMain refactored

---
+++ Client/SRC/Components/YQandA/Refactoring/Questions.jsx
@@ -0,0 +1,50 @@
+/*eslint-disable */
+
+import React, {useState, useEffect, useContext} from 'react';
+import axios from 'axios';
+import QAList from './QAList.jsx';
+import './styles.css';
+import {ContextObj} from '../../ContextObj';
+import QuestionSearch from './QuestionSearch.jsx';
+import QuestionModal from './QuestionModal.jsx';
+import { getServer, grabReviewScore, formatDate } from '../../helpers';
+
+const Questions = (props) => {
+  const {productInfo, productId} = useContext(ContextObj);
+  const [questions, setQuestions] = useState({results: []});
+  const [showQuestions, setShowQuestions] = useState(false);
+  const [newQuestion, setNewQuestion] = useState('');
+
+
+  useEffect(() => {
+    getServer(`/qa/questions?product_id=${productId}&count=100`)
+      .then((result) => setQuestions(result))
+      .catch((error) => console.log('questions get product id', error));
+  }, [productId, newQuestion]);
+
+
+
+  return (
+    <div className='questions'>
+      <div className="q-title">Questions & Answers</div>
+      <div className="qa-list-container">
+        <QuestionSearch questions={questions} addQuestion={<button className="q-button" onClick={() => setShowQuestions(true)}>Add A Question</button>}/>
+        <QuestionModal setNewQuestion={setNewQuestion} onClose={() => setShowQuestions(false)} show={showQuestions} name={productInfo.name} productId={productId}/>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
