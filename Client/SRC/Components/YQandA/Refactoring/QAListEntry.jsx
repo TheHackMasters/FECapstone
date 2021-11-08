3ba8f048fcfc1b70af98407f36deba84cc9788c3
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2021-11-08 13:28:48 -0600

    Question entry refactored

---
+++ Client/SRC/Components/YQandA/Refactoring/QAListEntry.jsx
@@ -0,0 +1,121 @@
+/*eslint-disable */
+
+import React, {useState, useEffect, useContext} from 'react';
+import axios from 'axios';
+//import {ContextObj} from '../../ContextObj';
+import AnswerEntry from './AnswerEntry.jsx';
+import AnswerModal from './AnswerModal.jsx';
+import { getServer, grabReviewScore, formatDate, putServer } from '../helpers';
+
+const QAListEntry = (props) => {
+
+  var listButton;
+  var questionBody = props.question.question_body;
+
+  const {productInfo, productId} = useContext(ContextObj);
+  const [answers, setAnswers] = useState({results: []});
+  const [showAnswers, setShowAnswers] = useState(false);
+  const [questionHelp, setQuestionHelp] = useState(false);
+  const [questionHelpfulness, setQuestionHelpfulness] = useState(props.question.question_helpfulness);
+  const [newAnswer, setNewAnswer] = useState('');
+  const [limit, setLimit] = useState(2);
+
+  var answerList = answers.results;
+  const questionId = props.question.question_id;
+
+  useEffect(() => {
+    getServer(`/qa/questions/${questionId}/answers?count=100`)
+      .then((result) => {
+        setAnswers(result);
+      })
+      .catch((error) => console.log('answers', error));
+  }, [questionId, newAnswer]);
+
+  useEffect(() => {
+    setLimit(2);
+  }, [productId]);
+
+  const listHandler = (array) => {
+    var clone = array.slice();
+    return clone.slice(0, limit);
+  };
+
+  const increaseLimit = () => {
+    setLimit(prevState => prevState + 1000);
+  };
+
+  const decreaseLimit = () => {
+    setLimit(2);
+  };
+
+  const updateQuestionHelp = () => {
+    if (!questionHelp) {
+      putServer(`/qa/questions/${questionId}/helpful`)
+        .then(() => { setQuestionHelp(true); })
+        .then(() => { setQuestionHelpfulness(prevState => prevState + 1); })
+        .catch((error) => console.log('helpfulness', error));
+    }
+  };
+
+  const sortAnswersBySeller = (answers) => {
+    var sellers = [];
+    var result = [];
+    for (var i = 0; i < answers.results.length; i++) {
+      var answer = answers.results[i];
+      if (answer.answerer_name === 'Seller' || answer.answerer_name === 'seller') {
+        sellers.push(answer);
+      } else {
+        result.push(answer);
+      }
+    }
+    for (var i = sellers.length - 1; i >= 0; i--) {
+      result.unshift(sellers[i]);
+    }
+    return result;
+  };
+
+  const highlightSearch = (string) => {
+    var clone = string.toLowerCase().slice();
+    var query = props.query.toLowerCase();
+    var result;
+    if (props.query.length >= 3) {
+      var index = clone.indexOf(query);
+      if (index > -1) {
+        result = <div>
+          <span>{string.slice(0, index)}</span>
+          <span className="highlight"><b>{string.slice(index, index + query.length)}</b></span>
+          <span>{string.slice(index + query.length)}</span>
+        </div>;
+      }
+      return result;
+    }
+    return string;
+  };
+
+  if (answerList.length - listHandler(answerList).length === 0 && answerList.length <= 2) {
+    listButton = null;
+  } else if (answerList.length - listHandler(answerList).length === 0 && answerList.length > 2) {
+    listButton = <div className="moreAnswers" onClick={decreaseLimit}>Collapse Answers</div>;
+  } else {
+    listButton = <div className="moreAnswers" onClick={increaseLimit}>See More Answers</div>;
+  }
+
+  return (
+    <div className="QAEntry">
+      <div className="qEntry">
+        <span className="qLetter">Q:</span> <span className="qText">{highlightSearch(questionBody)}</span>
+        <div className="sellerSig">
+          <span>    Helpful?</span>
+          <span className="link" onClick={updateQuestionHelp}> Yes ({questionHelpfulness})</span>
+          <span> | </span>
+          <span className="link" onClick={() => setShowAnswers(true)}> Add Answer</span>
+        </div>
+      </div>
+      <div><AnswerModal setNewAnswer={setNewAnswer} onClose={() => setShowAnswers(false)} name={productInfo.name} question={props.question.question_body} qId={props.question.question_id} show={showAnswers}/></div>
+      <div className="answerList">{listHandler(sortAnswersBySeller(answers)).map((answer, index) => <AnswerEntry answer={answer} key={index}/>)}</div>
+      {listButton}
+    </div>
+  );
+};
+
+export default QAListEntry;
---
+++ Client/SRC/Components/YQandA/Refactoring/QuestionModal.jsx
@@ -0,0 +1,110 @@
+/*eslint-disable */
+
+import React, {useState} from 'react';
+import { postServer, validateEmail } from '../helpers/helpers.js';
+
+const QuestionModal = (props) => {
+  const [qModalBody, setQModalBody] = useState('');
+  const [qModalName, setQModalName] = useState('');
+  const [qModalEmail, setQModalEmail] = useState('');
+
+  const qModalBodyHandler = () => {
+    setQModalBody(event.target.value);
+  };
+
+  const qModalNameHandler = () => {
+    setQModalName(event.target.value);
+  };
+
+  const qModalEmailHandler = () => {
+    setQModalEmail(event.target.value);
+  };
+
+  const submitQuestion = (body, name, email, id) => {
+
+    postServer('/qa/questions', {
+      body: body,
+      name: name,
+      email: email,
+      product_id: id
+    })
+      .then(() => props.setNewQuestion(JSON.stringify({
+        body: body,
+        name: name,
+        email: email,
+        product_id: id
+      })))
+      .then(() => props.onClose());
+
+  };
+
+  if (!props.show) {
+    return null;
+  }
+  var qWarning;
+  var nWarning;
+  var eWarning;
+
+  if (qModalBody.length > 0) {
+    qWarning = null;
+  } else {
+    qWarning = <span className="aWarningText">Please ask a question</span>;
+  }
+
+  if (qModalName.length > 0) {
+    nWarning = null;
+  } else {
+    nWarning = <span className='aNameText'>Please enter your nickname</span>;
+  }
+
+
+  if (validateEmail(qModalEmail)) {
+    eWarning = null;
+  } else {
+    eWarning = <span className='aEmailText'>Please enter a valid email</span>;
+  }
+
+  return (
+    <div className="q-modal" onClick={props.onClose}>
+      <div className="a-modal-content" onClick={e => e.stopPropagation()}>
+        <div className="a-modal-header">
+          <h2 className="a-modal-title">Ask Your Question about the... </h2>
+          <h4 className="questionSub"> {props.name}</h4>
+        </div>
+        <div className="a-modal-body">
+          <div className="a-text">
+            <label className="a-label">
+              * Your Question:
+            </label>
+            <textarea type="text" className="a-textarea" maxLength={1000} onChange={qModalBodyHandler}></textarea>
+          </div>
+          <div className="a-name">
+            <label className="a-label">
+              * What is your nickname?
+              <input className="a-name-box" maxLength={60} onChange={qModalNameHandler} type="text" placeholder="Example: jackson11!"></input>
+            </label>
+            <div className="warning">
+              For privacy reasons, do not use your full name or email address
+            </div>
+          </div>
+          <div>
+            <label className="a-label">
+              * Your email:
+              <input className="a-email-box" maxLength={60} onChange={qModalEmailHandler} type="text" placeholder="Why did you like the product or not?"></input>
+            </label>
+            <div className="warning">
+                For authentication reasons you will not be emailed
+            </div>
+            <div className="warning">* Indicates a required field</div>
+          </div>
+        </div>
+        <div className="a-modal-footer">
+          <button className="q-button" onClick={() => submitQuestion(qModalBody, qModalName, qModalEmail, props.productId)}>Submit</button>
+          {qWarning} {nWarning} {eWarning}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionModal;
