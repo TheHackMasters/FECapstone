86e0a8941fdac3828c3c644efc7c04065b51fbdb
Thistleman <codes5096@gmail.com>
2021-11-13 17:50:12 -0500

    Commit #94

--- Client/SRC/Components/DOverview/Checkbox.jsx
+++ Client/SRC/Components/DOverview/Checkbox.jsx
@@ -1,62 +1,68 @@
 import React from 'react';
 
 // shockingly complicated system to add a check mark
-// to a selected style, and hide it if not selected/
+// to a selected style, and hide it if not selected
 class Checkbox extends React.Component {
   constructor(props) {
     super(props);
-    const { id, styleItem } = props;
+    const { id } = props;
     this.state = {
       id,
-      display: 'block',
-      styleItem,
+      isSelected: true,
     };
   }
 
+  // checks the initial state of the app to render/hide the box
   componentDidMount() {
     const { styleItem } = this.props;
     const { id } = this.props;
     if (styleItem !== id) {
       this.setState({
-        display: 'none',
+        isSelected: false,
       });
     }
   }
 
+  // checks for changes and if the box needs to be displayed
   componentDidUpdate(prevProps) {
     const { styleItem } = this.props;
     const { id } = this.props;
     if (styleItem !== prevProps.styleItem) {
       if (styleItem === `${id}`) {
         this.setState({
-          display: 'block',
+          isSelected: true,
         });
       } else {
         this.setState({
-          display: 'none',
-          styleItem,
+          isSelected: false,
         });
       }
     }
   }
 
   render() {
-    const { id, display } = this.state;
+    const { id, display, isSelected } = this.state;
     const curDisplay = {
       position: 'absolute',
       top: '1em',
       right: '1em',
       cursor: 'pointer',
-      display,
     };
-    return (
-      <div>
-        <div
-          id={id}
-          style={curDisplay}
-        >
+    let checkmark;
+    // conditional rendering logic to display the checkbox
+    if (isSelected) {
+      checkmark = (
+        <div id={id} style={curDisplay}>
           ✅
         </div>
+      );
+    } else {
+      <div id={id} style={curDisplay} />;
+    }
+
+    return (
+      <div>
+        {checkmark}
       </div>
     );
   }
